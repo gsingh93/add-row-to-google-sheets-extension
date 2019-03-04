0eeeb914c3c9c6404356c41b1ce9fccd040767c2
Gulshan Singh <gsingh2011@gmail.com>
2019-03-03 23:54:20 -0800

    added a column selector to support arbitrary columns

--- popup.js
+++ popup.js
@@ -3,9 +3,10 @@ function setStatus(status) {
 }
 
 function appendToSheet(sheetId, row) {
+  var rangeEnd = String.fromCharCode('A'.charCodeAt(0) + row.length);
   var appendParams = {
     'spreadsheetId': sheetId,
-    'range': 'A:B',
+    'range': 'A:' + rangeEnd,
     'valueInputOption': 'RAW',
   };
   var valueRangeBody = {
@@ -26,20 +27,32 @@ function createSheetSelector(files) {
   return sheetSelector;
 }
 
-function addRowInputs(num_columns, contentDiv) {
-  for (var i = 0; i < num_columns; i++) {
-    var input = document.createElement('input');
+function addRowInputs(contentDiv) {
+  for (var i = 0; i < 26; i++) {
+    var input = document.createElement('textarea');
     input.id = 'col' + i;
+    input.style.display = 'none';
     contentDiv.appendChild(input);
   }
 }
 
-function createSubmitButton(num_columns, sheetSelector) {
+function showRowInputs(num_columns) {
+  for (var i = 0; i < 26; i++) {
+    var input = document.getElementById('col' + i);
+    if (i < num_columns) {
+      input.style.display = '';
+    } else {
+      input.style.display = 'none';
+    }
+  }
+}
+
+function createSubmitButton(numColumnsSelector, sheetSelector) {
   var submitButton = document.createElement('button');
   submitButton.innerText = 'Append Row';
   submitButton.onclick = function() {
     var row = [];
-    for (var i = 0; i < num_columns; i++) {
+    for (var i = 0; i < numColumnsSelector.value; i++) {
       row.push(document.getElementById('col' + i).value);
     }
 
@@ -48,7 +61,7 @@ function createSubmitButton(num_columns, sheetSelector) {
       .then(function() {
         setStatus('Successfully appended row');
         // Clear inputs
-        for (var i = 0; i < num_columns; i++) {
+        for (var i = 0; i < 26; i++) {
           var input = document.getElementById('col' + i);
           input.value = '';
         }
@@ -58,6 +71,18 @@ function createSubmitButton(num_columns, sheetSelector) {
       });
   };
   return submitButton;
+}
+
+function createNumColumnsSelector(contentDiv) {
+  var selector = document.createElement('select');
+  for (var i = 1; i <= 26; i++) {
+    selector.appendChild(new Option(i, i));
+  }
+  selector.value = 2;
+  selector.onchange = function () {
+    showRowInputs(selector.value);
+  };
+  return selector;
 }
 
 function main() {
@@ -83,14 +108,21 @@ function main() {
       contentDiv.appendChild(sheetSelector);
       contentDiv.appendChild(document.createElement('br'));
 
+      // Add num columns selector
+      var numColumnsSelector = createNumColumnsSelector();
+      var numColumnsText = document.createElement('p')
+      numColumnsText.innerText = 'Number of columns in row: ';
+      contentDiv.appendChild(numColumnsText);
+      contentDiv.appendChild(numColumnsSelector);
+      contentDiv.appendChild(document.createElement('br'));
+
       // Add row inputs
-      // TODO: Support arbitrary number of columns
-      var num_columns = 2;
-      addRowInputs(num_columns, contentDiv);
+      addRowInputs(contentDiv);
+      showRowInputs(numColumnsSelector.value);
       contentDiv.appendChild(document.createElement('br'));
 
       // Add submit button
-      var submitButton = createSubmitButton(num_columns, sheetSelector);
+      var submitButton = createSubmitButton(numColumnsSelector, sheetSelector);
       contentDiv.appendChild(submitButton);
     } else {
       setStatus('No Sheets found');
@@ -103,12 +135,17 @@ function main() {
 
 setStatus('Authenticating with Google Drive');
 chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-  gapi.load('client', function() {
-    gapi.client.setToken({access_token: token});
-    gapi.client.load('drive', 'v3', function() {
-      gapi.client.load('sheets', 'v4', function() {
-        main();
+  if (token === undefined) {
+    setStatus('Error authenticating with Google Drive');
+    console.log('Error authenticating with Google Drive');
+  } else {
+    gapi.load('client', function() {
+      gapi.client.setToken({access_token: token});
+      gapi.client.load('drive', 'v3', function() {
+        gapi.client.load('sheets', 'v4', function() {
+          main();
+        });
       });
     });
-  });
+  }
 });
